d169401efa53875ba546546ab0804945e36bc3cf
said1854 <msaliogullari@gmail.com>
2022-09-16 11:10:11 +0300

    template engine settings

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 import express from "express";
 const app = express();
-
 const port = 1854;
 
-app.get('/', (req, res) => {
-    res.send("Index Page")
-})
+// Ejs template engine
+app.set('view engine', 'ejs');
 
+//Static files middleware
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+app.get('/services', (req, res) => {
+    res.render('services');
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port:${port}`)
